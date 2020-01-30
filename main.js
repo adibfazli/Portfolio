a691513e3ff7aa81d0499d15be28abcd482cfbc7
Adib Fazli <adib.fazlii@gmail.com>
2020-01-30 12:22:28 -0800

    css

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ projects = [
         gitHub:'https://github.com/adibfazli/Zart-Message',
         link:'https://zart-message.herokuapp.com/login',
         image:'./image/zart.jpg',
+        technology:'./image/MERN.png',
     } ,
     {
         name:'Junkie',
@@ -12,6 +13,7 @@ projects = [
         gitHub:'https://github.com/adibfazli/junkie',
         link:'https://collection-junkie.herokuapp.com/',
         image:'./image/junkie.jpg',
+        technology:'./image/python.png',
     } ,
     {
         name:'You Pick',
@@ -19,6 +21,7 @@ projects = [
         gitHub:'https://github.com/adibfazli/youPick',
         link:'https://youpick2.herokuapp.com/',
         image:'./image/youpick.jpg',
+        technology:'./image/MEN.png',
     } ,
     {
         name:'Minesweeper',
@@ -26,6 +29,7 @@ projects = [
         gitHub:'https://github.com/adibfazli/Minesweeper',
         link:'',
         image:'./image/minesweeper.jpg',
+        technology:'./image/jsHtmlCss.png',
     }
 ]
 
@@ -41,39 +45,48 @@ function scrollFunction() {
 const container = document.querySelector('.postContainer')
 projects.forEach(element => {
 // <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> INFO
-    const info = document.createElement('div')
-    info.classList.add('postInfoDiv')
+    const info = document.createElement('div');
+    info.classList.add('postInfoDiv');
 
-        const name = document.createElement('a')
-        name.classList.add('projectName')
+        const name = document.createElement('a');
+        name.classList.add('projectName');
         name.textContent = `${element.name}`
-        name.setAttribute('href' , `${element.link}`)
+        name.setAttribute('href' , `${element.link}`);
         info.appendChild(name)
 
-        const gitA = document.createElement('a')
-        const gitImg = document.createElement('img')
-        gitImg.classList.add('gitImg')
-        gitImg.setAttribute('src',`${element.gitHubImage}`)
-        gitImg.setAttribute('href',`${element.gitHub}`)
+        const gitA = document.createElement('a');
+        const gitImg = document.createElement('img');
+        gitImg.classList.add('gitImg');
+        gitImg.setAttribute('src',`${element.gitHubImage}`);
+        gitImg.setAttribute('href',`${element.gitHub}`);
         gitA.appendChild(gitImg)
-        info.appendChild(gitA)
+        
 
-        const gitHubLink = document.createElement('a')
-        gitHubLink.classList.add('gitHubLink')
+        const gitHubLink = document.createElement('a');
+        const gitDiv = document.createElement('div');
+        gitDiv.classList.add('gitDiv')
+        gitHubLink.classList.add('gitHubLink');
         gitHubLink.textContent = 'GitHub'
-        gitHubLink.setAttribute('href' , `${element.gitHub}`)
-        info.appendChild(gitHubLink)
+        gitHubLink.setAttribute('href' , `${element.gitHub}`);
+        gitDiv.appendChild(gitImg)
+        gitDiv.appendChild(gitHubLink)
+        info.appendChild(gitDiv)
+
+        const stack = document.createElement('img');
+        stack.classList.add('stack')
+        stack.setAttribute('src' , `${element.technology}`);
+        info.appendChild(stack)
 // <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> SHOT
-    const shot = document.createElement('div')
-    shot.classList.add('postShotDiv')
-        const projectShot = document.createElement('img')
-        projectShot.setAttribute('src' , `${element.image}`)
+    const shot = document.createElement('div');
+    shot.classList.add('postShotDiv');
+        const projectShot = document.createElement('img');
+        projectShot.setAttribute('src' , `${element.image}`);
         projectShot.classList.add('postImg')
         shot.appendChild(projectShot)
 // <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> POST
-    post = document.createElement('div')
-    post.classList.add('post')
-    post.appendChild(info)
-    post.appendChild(shot)
-    container.appendChild(post)
+    post = document.createElement('div');
+    post.classList.add('post');
+    post.appendChild(info);
+    post.appendChild(shot);
+    container.appendChild(post);
 });
